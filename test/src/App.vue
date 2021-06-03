49d4b654ee6cf5b862087a782e8484c7249bd0d4
akadashiso <akadashiso@gmail.com>
2021-06-03 19:16:49 +0900

    [Start]Section6-79 : SFC (SingleFileComponent)

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TestComponent />
+    <HelloWorld msg="Welcome" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import TestComponent from "./modules/TestComponent.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    TestComponent,
   },
 };
 </script>
---
+++ test/src/modules/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    テストです。
+    {{ testData }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  data() {
+    return {
+      testData: "テストデータです。",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
